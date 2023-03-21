68d16ce7645d5c109f36a56a7df26b25b05c7dcd
Q1unt, Дмитрий <dima.kunitsien.03@gmail.com>
2023-03-22 00:20:01 +0300

    modified select queries

--- Заполнение таблиц INSERT - запросы.sql
+++ Заполнение таблиц INSERT - запросы.sql
@@ -1,202 +1,124 @@
 --Заполнение таблицы жанры
-INSERT INTO genre
-	VALUES (1,'Rep');
-INSERT INTO genre
-	VALUES (2,'K-pop');
-INSERT INTO genre
-	VALUES (3,'Rock');
-INSERT INTO genre
-	VALUES (4, 'Edm');
-INSERT INTO genre
-	VALUES (5,'classical music');
+INSERT INTO genre(name) VALUES
+	('Rep'),
+	('K-pop'),
+	('Rock'),
+	('Edm'),
+	('classical music');
 
 --Заполнение таблицы артист
-INSERT INTO artist
-	VALUES (1,'Eгор крид');
-INSERT INTO artist
-	VALUES (2,'EMINEM');
-INSERT INTO artist
-	VALUES (3,'BLACKPINK');
-INSERT INTO artist
-	VALUES (4,'EXO');
-INSERT INTO artist
-	VALUES (5,'Skrillex');
-INSERT INTO artist
-	VALUES (6,'Marshmello');
-INSERT INTO artist
-	VALUES (7,'Skillet');
-INSERT INTO artist
-	VALUES (8,'Bring Me The Horizon');
-INSERT INTO artist
-	VALUES (9,'Mozart');
+INSERT INTO artist(name) VALUES
+	('Eгор крид'),
+	('EMINEM'),
+	('BLACKPINK'),
+	('EXO'),
+	('Skrillex'),
+	('Marshmello'),
+	('Skillet'),
+	('Bring Me The Horizon'),
+	('Mozart'),
 
 --Заполнение таблицы альбом
-INSERT INTO album 
-	VALUES (1,'PU$$Y BOY', 2021);
-INSERT INTO album 
-	VALUES (2,'Kamikaze', 2018);
-INSERT INTO album 
-	VALUES (3,'BORN PINK', 2022);
-INSERT INTO album 
-	VALUES (4,'LOVE SHOT – The 5th Album Repackage', 2018);
-INSERT INTO album 
-	VALUES (5,'Bangarang', 2011);
-INSERT INTO album 
-	VALUES (6,'Joytime II', 2018);
-INSERT INTO album 
-	VALUES (7,'Comatose', 2006);
-INSERT INTO album 
-	VALUES (8,'Sempiternal (Expanded Edition)', 2013);
-INSERT INTO album 
-	VALUES (9,'Моцарт. Колыбельная песня - классическая музыка', 2013);
+INSERT INTO album(name, year_of_issue) VALUES
+	('PU$$Y BOY', 2021),
+	('Kamikaze', 2018),
+	('BORN PINK', 2022),
+	('LOVE SHOT – The 5th Album Repackage', 2018),
+ 	('Bangarang', 2011),
+ 	('Joytime II', 2018),
+	('Comatose', 2006),
+	('Sempiternal (Expanded Edition)', 2013),
+	('Моцарт. Колыбельная песня - классическая музыка', 2013),
 
 --Заполнение таблицы артист-жанр
-INSERT INTO artist_genre 
-	VALUES (1, 1, 1);
-INSERT INTO artist_genre
-	VALUES (2, 1, 2);
-INSERT INTO artist_genre
-	VALUES (3, 2, 3);
-INSERT INTO artist_genre
-	VALUES (4, 2, 4);
-INSERT INTO artist_genre
-	VALUES (5, 4, 5);
-INSERT INTO artist_genre
-	VALUES (6, 4, 6);
-INSERT INTO artist_genre
-	VALUES (7, 3, 7);
-INSERT INTO artist_genre
-	VALUES (8, 3, 8);
-INSERT INTO artist_genre
-	VALUES (9, 5, 9);
-INSERT INTO artist_genre
-	VALUES (10, 4, 1);
-INSERT INTO artist_genre
-	VALUES (11, 5, 7);
-INSERT INTO artist_genre
-	VALUES (12, 4, 5);
-
+INSERT INTO artist_genre(genre_id, artist_id) VALUES
+	(1, 1),
+	(1, 2),
+	(2, 3),
+	(2, 4),
+	(4, 5),
+	(4, 6),
+	(3, 7),
+	(3, 8),
+	(5, 9),
+	(4, 1),
+	(5, 7),
+	(3, 5);
 --Заполнение таблицы артист-альбом
-INSERT INTO artist_album 
-	VALUES (1, 1, 1);
-INSERT INTO artist_album 
-	VALUES (2, 2, 2);
-INSERT INTO artist_album 
-	VALUES (3, 3, 3);
-INSERT INTO artist_album 
-	VALUES (4, 4, 4);
-INSERT INTO artist_album 
-	VALUES (5, 5, 5);
-INSERT INTO artist_album 
-	VALUES (6, 6, 6);
-INSERT INTO artist_album 
-	VALUES (7, 7, 7);
-INSERT INTO artist_album 
-	VALUES (8, 8, 8);
-INSERT INTO artist_album 
-	VALUES (9, 9, 9);
+--INSERT INTO artist_album(artist_id, albun_id) VALUES
+--	(1, 1),
+--	(2, 2),
+--	(3, 3),
+--	(4, 4),
+--	(5, 5),
+--	(6, 6),
+--	(7, 7),
+--	(8, 8),
+--	(9, 9);
+INSERT INTO artist_album
+	VALUES(10, 5, 5);
 
 --Заполнение таблицы трек
-INSERT INTO track  
-	VALUES (1, 'PU$$Y BOY', 178, 1);
-INSERT INTO track 
-	VALUES (2, 'ТЕЛЕФОН', 126, 1);
-INSERT INTO track 
-	VALUES (3, 'Fall',263, 2);
-INSERT INTO track 
-	VALUES (4, 'Venom',270, 2);
-INSERT INTO track 
-	VALUES (5, 'Pink Venom',187, 3);
-INSERT INTO track 
-	VALUES (6, 'Shut Down', 176, 3);
-INSERT INTO track 
-	VALUES (7, 'Love Shot',210, 4);
-INSERT INTO track  
-	VALUES (8, 'Wait',249, 4);
-INSERT INTO track 
-	VALUES (9, 'Bangarang',219, 5);
-INSERT INTO track 
-	VALUES (10, 'Kyoto',201, 5);
-INSERT INTO track
-	VALUES (11, 'Stars',210, 6);
-INSERT INTO track 
-	VALUES (12, 'Together',228, 6);
-INSERT INTO track 
-	VALUES (13, 'Comatose',231, 7);
-INSERT INTO track 
-	VALUES (14, 'Falling Inside the Black',211 , 7);
-INSERT INTO track 
-	VALUES (15, 'Can You Feel My Heart',249 , 8);
-INSERT INTO track
-	VALUES (16, 'Sleepwalking',237 , 8);
-INSERT INTO track
-	VALUES (17, 'Oh My Darling Clementine',99 , 9);
-INSERT INTO track
-	VALUES (18, 'Спокойной Ночи',186 , 9);
-INSERT INTO track
-	VALUES (19, 'Together',228, 5);
-INSERT INTO track
-	VALUES (20, 'Can You Feel My Heart',249 , 7);
-INSERT INTO track
-	VALUES (21, 'Shut Down', 176, 1);
-INSERT INTO track
-	VALUES (22, 'PU$$Y BOY', 178, 4););
+INSERT INTO track(name, duration, album_id)  VALUES
+	('PU$$Y BOY', 178, 1),
+	('ТЕЛЕФОН', 126, 1),
+	('Fall',263, 2),
+	('Venom',270, 2),
+	('Pink Venom',187, 3),
+	('Shut Down', 176, 3),
+	('Love Shot',210, 4),
+	('Wait',249, 4),
+	('Bangarang',219, 5),
+	('Kyoto',201, 5),
+	('Stars',210, 6),
+	('Together',228, 6),
+	('Comatose',231, 7),
+	('Falling Inside the Black',211 , 7),
+	('Can You Feel My Heart',249 , 8),
+	('Sleepwalking',237 , 8),
+	('Oh My Darling Clementine',99 , 9),
+	('Спокойной Ночи',186 , 9),
+	('Always', 234, 5),
+	('Can You Feel',252 , 7),
+	('shot', 170, 1),
+	('BOY', 176, 4);
 
 --Заполнение таблицы сборник
-INSERT INTO compilation 
-	VALUES (1,'Incendiary hits', 2015);
-INSERT INTO compilation 
-	VALUES (2,'Music for relaxation', 2010);
-INSERT INTO compilation 
-	VALUES (3,'Deep hit parade', 2020);
-INSERT INTO compilation 
-	VALUES (4,'music for sex', 2016);
-INSERT INTO compilation 
-	VALUES (5,'dance music', 2018 );
-INSERT INTO compilation 
-	VALUES (6,'turnip stars', 2012 );
-INSERT INTO compilation 
-	VALUES (7,'best rock', 2013 );
-INSERT INTO compilation 
-	VALUES (8,'workout music', 2021);
+INSERT INTO compilation(name, year_of_issue) VALUES
+	('Incendiary hits', 2015),
+	('Music for relaxation', 2010),
+	('Deep hit parade', 2020),
+	('music for sex', 2016),
+	('dance music', 2018 ),
+	('turnip stars', 2012 ),
+	('best rock', 2013 ),
+	('workout music', 2021);
 
 --Заполнение таблицы сборнике музыки
-INSERT INTO Мusic_collection 
-	VALUES (1, 5, 1);
-INSERT INTO Мusic_collection 
-	VALUES (2, 10, 1);
-INSERT INTO Мusic_collection 
-	VALUES (3, 2, 2);
-INSERT INTO Мusic_collection 
-	VALUES (4, 2, 3);
-INSERT INTO Мusic_collection 
-	VALUES (5, 16, 3);
-INSERT INTO Мusic_collection 
-	VALUES (6, 18, 4);
-INSERT INTO Мusic_collection 
-	VALUES (7, 17, 4);
-INSERT INTO Мusic_collection 
-	VALUES (8, 13, 5);
-INSERT INTO Мusic_collection 
-	VALUES (9, 10, 6);
-INSERT INTO Мusic_collection 
-	VALUES (10, 2, 6);
-INSERT INTO Мusic_collection 
-	VALUES (11, 4, 7);
-INSERT INTO Мusic_collection 
-	VALUES (12, 7, 7);
-INSERT INTO Мusic_collection 
-	VALUES (13, 5, 8);
+INSERT INTO Мusic_collection(track_id, compilation_id) VALUES
+	(5, 1),
+	(10, 1),
+	(2, 2),
+	(2, 3),
+	(16, 3),
+	(18, 4),
+	(17, 4),
+	(13, 5),
+	(10, 6),
+	(2, 6),
+	(4, 7),
+	(7, 7),
+	(5, 8);
 
 --SELECT Запрос для просмота таблицы
 --SELECT * FROM Таблицу
 
 --Удаление таблицы по айди
---DELETE FROM track  
---WHERE id = 
+--DELETE FROM artist_album 
+--WHERE id = 10
 
 -- Изменения в таблицы
---UPDATE album
+--UPDATE artist_genre
 --SET year_of_issue = 2020
 --WHERE id = 2;
 
--- Написание SELECT - запросов.sql
+++ Написание SELECT - запросов.sql
@@ -2,21 +2,26 @@
 SELECT name, year_of_issue FROM album
 WHERE year_of_issue = 2018;
 
+
 -- Название и продолжительность самого длительного трека.
 SELECT name, duration FROM track
 WHERE duration = (SELECT MAX(duration) FROM track)
+
 
 -- Название треков, продолжительность которых не менее 3,5 минут.
 SELECT name, duration FROM track
 WHERE duration > 210;
 
+
 -- Названия сборников, вышедших в период с 2018 по 2020 год включительно.
 SELECT name, year_of_issue FROM compilation
 WHERE year_of_issue BETWEEN 2018 AND 2020;
 
+
 -- Исполнители, чьё имя состоит из одного слова.
 SELECT name FROM artist
 WHERE name NOT LIKE '% %';
+
 
 -- Название треков, которые содержат слово «мой» или «my».
 SELECT name FROM track
@@ -28,22 +33,28 @@ SELECT count(ag.genre_id), g.name AS name_genre FROM artist_genre ag
 JOIN genre g ON ag.genre_id = g.id
 GROUP BY g.name;
 
+
 --Количество треков, вошедших в альбомы 2019–2020 годов.
-SELECT count(t.name), a.year_of_issue  FROM track t 
+SELECT count(t.name) FROM track t 
 JOIN album a ON t.album_id = a.id
-WHERE a.year_of_issue BETWEEN 2019 AND 2020
-GROUP BY a.year_of_issue;
+WHERE a.year_of_issue BETWEEN 2019 AND 2020;
+
 
 --Средняя продолжительность треков по каждому альбому.
 SELECT a.year_of_issue, AVG(t.duration)  FROM track t
 JOIN album a ON t.album_id = a.id
 GROUP BY a.year_of_issue;
 
+
 --Все исполнители, которые не выпустили альбомы в 2020 году.
-SELECT a.name AS name_artist, a.year_of_issue AS year_of_issue FROM artist_album aa 
-JOIN album a ON aa.albun_id = a.id
-WHERE a.year_of_issue != 2020
-GROUP BY a.name, a.year_of_issue;
+SELECT a.name FROM artist a 
+WHERE a.name NOT IN (
+	SELECT a2.name FROM artist a2 
+	JOIN artist_album aa ON a2.id = aa.id 
+	JOIN album a3 ON aa.albun_id = a3.id
+	WHERE a3.year_of_issue = 2020
+);
+
 
 --Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 SELECT c.name AS name_compilation  FROM Мusic_collection mc
@@ -54,21 +65,22 @@ JOIN artist_album aa ON a.id = aa.id
 JOIN artist a2 ON aa.artist_id = a2.id
 WHERE a2.name = 'Bring Me The Horizon';
 
+
 --Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
-SELECT count(g.name), a2.name AS album FROM artist_genre ag 
-JOIN artist a ON ag.artist_id = a.id 
-JOIN genre g ON ag.genre_id = g.id
+SELECT DISTINCT a2.name AS album, count(ag.genre_id), ag.artist_id FROM artist_genre ag
+JOIN artist a ON ag.artist_id = a.id
 JOIN artist_album aa ON a.id = aa.id
 JOIN album a2 ON aa.albun_id = a2.id
-GROUP BY a2.name
-HAVING count(g.name) > 1;
+GROUP BY a2.name, ag.artist_id
+HAVING count(ag.genre_id) > 1;
+
+
 
 --Наименования треков, которые не входят в сборники.
-SELECT t.name AS track, count(c.name) AS compilation FROM Мusic_collection mc
-JOIN compilation c ON mc.compilation_id = c.id 
-FULL JOIN track t ON mc.track_id = t.id
-GROUP BY t.name
-HAVING count(c.name) = 0;
+SELECT t.name AS track FROM track t 
+LEFT JOIN Мusic_collection mc ON t.id = mc.id 
+WHERE mc.track_id IS NULL;
+
 
 --Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
 SELECT t.duration, a2.name  FROM track t 
@@ -76,13 +88,18 @@ JOIN album a ON t.album_id = a.id
 JOIN artist_album aa ON a.id = aa.id 
 JOIN artist  a2 ON aa.albun_id = a2.id
 WHERE duration = (SELECT MIN(duration) FROM track)
-GROUP BY a2.name, t.duration
+
 
 --Названия альбомов, содержащих наименьшее количество треков.
-SELECT a2.name AS album, count(t.name) AS track FROM track t 
-JOIN album a ON t.album_id = a.id 
-JOIN artist_album aa ON a.id = aa.id 
-JOIN album a2 ON aa.albun_id = a2.id
-GROUP BY a2.name
-ORDER BY count(t.name) asc;
+SELECT a.name, count(*) FROM track t
+JOIN album a ON t.album_id = a.id
+GROUP BY a.name
+HAVING count(*) = (
+	SELECT count(t2.name) FROM track t2
+	JOIN album a2  ON t2.album_id  = a2.id
+	GROUP BY a2.name
+	ORDER BY count(t2.name)
+	LIMIT 1
+);
+
 
